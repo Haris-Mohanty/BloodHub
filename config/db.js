90feb37aaab3ae9d41e206f8d761999bb5de87c4
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-16 00:10:51 +0530

    db.js- connectDB function code fixed.

--- config/db.js
+++ config/db.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 
 const connectDB = async () => {
-    
+    try{
+        mongoose.connect(process.env.MONGO_URL);
+        console.log(`Connected to MongoDB Database ${mongoose.connection.host}`);
+    } catch(error){
+        console.log(`MongoDB Database Error ${error}`.bgRed.white);
+    }
 };
 
 module.exports = connectDB;
