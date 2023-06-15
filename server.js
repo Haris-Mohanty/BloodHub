b26422e493fc19a91852427ef1fd397bdbe0bbaa
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-16 00:06:27 +0530

    Create a config folder and add db.js file.

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ const colors = require("colors");
 const morgan = require("morgan");
 const cors = require("cors");
 
-//*** MIDDLEWARE */
-app.use(express.json);
-app.use(cors());
-app.use(morgan("dev"));
 
 //Dot Config
 dotenv.config();
 
 //***** REST OBJECT ****/
 const app = express();
+
+
+//*** MIDDLEWARE */
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
 
 //***** ROUTES ****/
 //1 test routes
@@ -24,5 +27,5 @@ const PORT = process.env.PORT || 8080;
 
 //***** LISTEN ******/
 app.listen(PORT, () => {
-  console.log("Node Server Running");
+  console.log(`Node Server Running In ${process.env.DEV_MODE} mode on Port ${process.env.PORT}`.bgBlue.white);
 });
