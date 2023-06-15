96349a318a0cb9624e0e423384eaff717c9f9ccb
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-15 23:49:41 +0530

    server.js- cors required.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
+const cors = require("cors");
 
 
 //Dot Config
