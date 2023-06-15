54c3676c4cd95d7e8720a32ef5b9b822afb4386b
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-15 22:59:08 +0530

    server.js - send 200 status successfully.

--- server.js
+++ server.js
@@ -3,17 +3,20 @@ const express = require("express");
 //***** REST OBJECT ****/
 const app = express();
 
-
 //***** ROUTES ****/
 //1 test routes
-app.get("/test");
-
+app.get("/", (req, res) => {
+    res.status(200).json({
+      message : "Welcome to BloodHub."
+    });
+});
 
 
 //***** PORT *****/
 const PORT = 8080;
 
+
 //***** LISTEN ******/
 app.listen(PORT, () => {
-    console.log("Node Server Running");
+  console.log("Node Server Running");
 });
