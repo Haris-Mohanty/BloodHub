5824ba27cdcb979e151d07f4167e95d3276c97bb
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-15 23:15:02 +0530

    routes- testroutes.js- testController.js imported.

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const { testController } = require("../controller/testController.js");
 
 //ROUTER OBJECT
 const router = express.Router();
 
 //Routes Create
-router.get("/test", );
+router.get("/test", testController);
