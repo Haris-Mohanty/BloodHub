7178f3e45d9237fc183bd2c71e693c5a8f77f2ff
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-16 23:15:55 +0530

    server.js- Routes added.

--- server.js
+++ server.js
@@ -5,32 +5,31 @@ const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
-
 //Dot Config
 dotenv.config();
 
 //** MongoDb Connection ***/
 connectDB();
 
-
 //***** REST OBJECT ****/
 const app = express();
-
 
 //*** MIDDLEWARE */
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-
 //***** ROUTES ****/
-//1 test routes
 app.use("/api/v1/test", require("./routes/testRoutes"));
+app.use("api/v1/auth", require("./routes/authRoutes"));
 
 //***** PORT *****/
 const PORT = process.env.PORT || 8080;
 
 //***** LISTEN ******/
 app.listen(PORT, () => {
-  console.log(`Node Server Running In ${process.env.DEV_MODE} mode on Port ${process.env.PORT}`.bgBlue.white);
+  console.log(
+    `Node Server Running In ${process.env.DEV_MODE} mode on Port ${process.env.PORT}`
+      .bgBlue.white
+  );
 });
