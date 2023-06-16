57e4abed10b0f53af132e92f7ef06681f448f141
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-16 23:11:49 +0530

    controller- authController.js- password hashed successfully.

--- controller/authController.js
+++ controller/authController.js
@@ -5,14 +5,17 @@ const registerController = async (req, res) => {
   try {
     const existingUser = await userModel.findOne({ email: req.body.email });
 
-    //VALIDATION
+    //**** VALIDATION ****/
     if (existingUser) {
       return res.status(200).send({
         success: false,
         message: "User Already Exist!",
       });
     }
-
+    //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
   } catch (err) {
     console.log(err);
     res.status(500).send({
