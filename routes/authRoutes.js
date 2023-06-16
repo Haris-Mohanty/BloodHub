1f6dcf463468cf28c7c3b1a7c6244385ae11804e
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-16 23:00:20 +0530

    routes- authRoutes.js- registerCntroller imported.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const { registerController } = require("../controller/authController");
 
 //ROUTER OBJECT
 const router = express.Router();
 
 //Routes
-router.post("/register");
+router.post("/register", registerController);
 
 //Exports
 module.exports = router;
