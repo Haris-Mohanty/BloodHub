f0570635df5c9d4ca67e64c0b9f711e1b093485b
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-17 11:55:33 +0530

    middleware- authMiddleware.js- route protect by middleware code fixed.

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,8 +3,16 @@ const jwt = require("jsonwebtoken");
 module.exports = async (req, res, next) => {
   try {
     const token = req.headers("authorization").split(" ")[1];
-    jwt.verify(token, process.env.JWT_SECRET, () => {
-        
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        return res.status(401).send({
+          success: false,
+          message: "Auth Failed!",
+        });
+      } else {
+        req.body.userId = decode.id;
+        next();
+      }
     });
   } catch (error) {
     console.log(error);
