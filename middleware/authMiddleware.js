251f861d6c60f59d08fc9373329ae825c9df5c45
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-17 11:53:11 +0530

    middleware- authMiddleware.js- jwt verify code processing....

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,17 @@
 const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.headers("authorization").split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET, () => {
+        
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(401).send({
+      success: false,
+      error,
+      message: "Auth Failed!",
+    });
+  }
+};
