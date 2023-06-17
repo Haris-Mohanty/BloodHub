198ca4e7584be4348e864f5d77505e9c10e91b73
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-17 12:58:57 +0530

    User Fetched Successfully.

--- controller/authController.js
+++ controller/authController.js
@@ -84,7 +84,12 @@ const loginController = async (req, res) => {
 const currentUserController = async (req, res) => {
   try {
 
-    const 
+    const user = await userModel.findOne({ _id: req.body.userId });
+    return res.status(200).send({
+      success: true,
+      message: "User Fetched Successfully!",
+      user
+    });
 
   } catch(error){
     console.log(error);
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
   try {
-    const token = req.headers("authorization").split(" ")[1];
+    const token = req.headers["authorization"].split(" ")[1];
     jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
       if (err) {
         return res.status(401).send({
@@ -10,7 +10,7 @@ module.exports = async (req, res, next) => {
           message: "Auth Failed!",
         });
       } else {
-        req.body.userId = decode.id;
+        req.body.userId = decode.userId;
         next();
       }
     });
