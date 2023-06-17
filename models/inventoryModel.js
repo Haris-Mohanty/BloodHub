82c2ba75a42d73f39f9d477566945ffa77a71a09
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-17 13:04:05 +0530

    models- inverntoryModels.js- create inverntoryschema code rpocessing...

--- models/inventoryModel.js
+++ models/inventoryModel.js
@@ -1,2 +1,8 @@
 const mongoose = require("mongoose");
 
+const inventorySchema = new mongoose.Schema({
+    
+});
+
+//****** EXPORT *****/
+module.exports = mongoose.model("Inventory", inventorySchema);
