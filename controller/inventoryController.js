a289947ecfc293b7372dd9353006c490ab7e180c
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-19 11:42:34 +0530

    controller- inventoryController.js- createInventoryController function code fixed.

--- controller/inventoryController.js
+++ controller/inventoryController.js
@@ -1,7 +1,30 @@
+const inventoryModel = require("../models/inventoryModel");
+const userModel = require("../models/userModel");
+
 //********* CREATE INVENTORY *******/
 const createInventoryController = async (req, res) => {
   try {
-    
+    const { email, inventoryType } = req.body;
+    //Validation
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      throw new Error("User not Found!");
+    }
+    //Check inventoryType
+    if (inventoryType === "in" && user.role !== "donor") {
+      throw new Error("Not a Donor Account!");
+    }
+    if (inventoryType === "out" && user.role !== "hospital") {
+      throw new Error("Not a Hospital!");
+    }
+    //Save inventory
+    const inventory = new inventoryModel(req.body);
+    await inventory.save();
+    //Response
+    return res.status(201).send({
+      success: true,
+      message: "New Blood Record Added!",
+    });
   } catch (err) {
     console.log(err);
     return res.status(500).send({
@@ -12,4 +35,5 @@ const createInventoryController = async (req, res) => {
   }
 };
 
+//****** EXPORT *****/
 module.exports = { createInventoryController };
