e3432284fa2cd572c25e419133249bf8b89257cc
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-19 11:23:30 +0530

    controller- inventoryController.js- createInventoryController function code processing....

--- controller/inventoryController.js
+++ controller/inventoryController.js
@@ -1,3 +1,15 @@
-const createInventoryController = (req, res) => {};
+//********* CREATE INVENTORY *******/
+const createInventoryController = async (req, res) => {
+  try {
+    
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({
+      success: false,
+      message: "Error in Create Inventory API!",
+      err,
+    });
+  }
+};
 
 module.exports = { createInventoryController };
