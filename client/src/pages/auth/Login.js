4069151e6b1a44a8817c46361c32045409bdf692
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-21 17:38:39 +0530

    client- src- pages- auth- Login.js- Login form created.

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -9,34 +9,23 @@ const Login = () => {
             <img src="./assets/images/login.jpg" />
           </div>
           <div className="col-md-4 form-container">
-            //******* LOGIN FORM CODE START */
+            
             <form>
               <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">
-                  Email address
-                </label>
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
                 <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                <div id="emailHelp" className="form-text">
-                  We'll never share your email with anyone else.
-                </div>
               </div>
+
               <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">
-                  Password
-                </label>
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
                 <input type="password" className="form-control" id="exampleInputPassword1" />
               </div>
-              <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" htmlFor="exampleCheck1">
-                  Check me out
-                </label>
-              </div>
+
               <button type="submit" className="btn btn-primary">
                 Submit
               </button>
             </form>
-            //******* LOGIN FORM CODE END */
+            
           </div>
         </div>
       </div>
