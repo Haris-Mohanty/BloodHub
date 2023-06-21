60938836f338bf28da1874d311345496106369ac
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-21 17:01:09 +0530

    client- src- app.js- Routing created.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
