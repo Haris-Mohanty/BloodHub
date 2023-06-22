7b749121e3994ac0f6744a757fe35bb13c02a657
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-22 15:39:00 +0530

    client- src- components- Form- form.js- login and register data gets successfully.

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,5 +1,43 @@
 export const handleLogin = (e, email, password, role) => {
-    
+  e.preventDefault();
+  try {
+    if (!role || !email || !password) {
+      return alert("empty");
+    }
+    console.log("login", e, email, password, role);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-export const handleRegister = () => {};
+export const handleRegister = (
+  e,
+  name,
+  role,
+  email,
+  password,
+  organisationName,
+  hospitalName,
+  website,
+  address,
+  phone
+) => {
+  e.preventDefault();
+  try {
+    console.log(
+      "register",
+      e,
+      name,
+      role,
+      email,
+      password,
+      organisationName,
+      hospitalName,
+      website,
+      address,
+      phone
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
