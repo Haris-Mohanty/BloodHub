472a743b736811d144740ef11748991499b4732c
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-23 12:41:20 +0530

    client- src- services- API.js- interceptor created successfully.

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
 
-API.interceptors.request.use((req,) => {
-
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("token")) {
+    req.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  }
+  return req;
 });
+
+export default API;
