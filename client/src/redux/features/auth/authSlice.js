7c3ba925c9a65abd41815cdda0c8ecb6bce3a098
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-23 12:16:12 +0530

    client- src- redux- features- auth- authSlice.js- create authSlice object code processing.....

--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -1,1 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  loading: false,
+  user: null,
+  token: null,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  
+});
