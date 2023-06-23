fa3fe3309e9b9df627aaba111af7dbb68b54660c
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-23 12:17:56 +0530

    client- src- redux- store.js- add reducers.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./features/auth/authSlice";
 
 const store = configureStore({
   reducer: {
-    
+    auth: authSlice,
   },
 });
 
