705c712568d82bf284773408e4f5230978c9de70
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-24 12:43:43 +0530

    client- src- redux- features- auth- authAction.js- userRegister function created & exported.

--- client/src/redux/features/auth/authAction.js
+++ client/src/redux/features/auth/authAction.js
@@ -25,4 +25,24 @@ export const userLogin = createAsyncThunk(
 );
 
 //****** REGISTER **********/
-export const userRegister = createAsyncThunk("auth/register", async({}));
+export const userRegister = createAsyncThunk(
+  "auth/register",
+  async ({ name, role, email, password, organisationName, hospitalName, website, address, phone,}, { rejectWithValue } ) => {
+
+    try {
+      const { data } = await API.post("/auth/register", {name, role, email, password, organisationName, hospitalName, website, address, phone});
+      if(data.success){
+        toast.success(data.message);
+        window.location.replace("/login");
+      }
+
+    } catch (error) {
+      console.log(error);
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);
