71998d642cee33e0250b8e019a9ba9fe7ce46743
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-24 11:30:40 +0530

    npm i react-toastify- Show messages.

--- client/src/redux/features/auth/authAction.js
+++ client/src/redux/features/auth/authAction.js
@@ -1,6 +1,21 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import API from "../../../services/API";
 
 export const userLogin = createAsyncThunk(
   "auth/login",
-  async ({ role, email, password }, { rejectWithValue }) => {}
+  async ({ role, email, password }, { rejectWithValue }) => {
+    try {
+      const { data } = await API.post("/auth/login", { role, email, password });
+      //Store Token
+      if (data.success) {
+        localStorage.setItem("token", data.token);
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
 );
