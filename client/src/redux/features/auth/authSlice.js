59d88eac6b8140342bead1df2453fd85edf56720
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-24 12:44:37 +0530

    client- src- redux- features- auth- authslice.js- Add slice for register.

--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -17,6 +17,21 @@ const authSlice = createSlice({
   initialState: initialState,
   reducers: {},
   extraReducers: (builder) => {
+    //Login User
+    builder.addCase(userLogin.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(userLogin.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.user = payload.user;
+      state.token = payload.token;
+    });
+    builder.addCase(userLogin.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    });
+    //Register User
     builder.addCase(userLogin.pending, (state) => {
       state.loading = true;
       state.error = null;
