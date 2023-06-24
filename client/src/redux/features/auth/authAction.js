32c4521789959e65b70f4855da347d78c409d6cf
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-24 11:33:26 +0530

    client- src- redux- features- auth- authAction.js- toast imported.

--- client/src/redux/features/auth/authAction.js
+++ client/src/redux/features/auth/authAction.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import API from "../../../services/API";
+import { toast } from "react-toastify";
 
 export const userLogin = createAsyncThunk(
   "auth/login",
