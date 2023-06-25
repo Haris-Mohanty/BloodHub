e4626a483f29dcacc80dbf89c8426fcb6c2e8b27
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-25 18:02:39 +0530

    client- src- components- pages- Homepage.js- Loading spinner add code fixed.

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Spinner from "../components/shared/Spinner";
+import { toast } from "react-toastify";
 
 const HomePage = () => {
   const { loading, error } = useSelector((state) => state.auth);
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      {error && <span>{toast.error(error)}</span>}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <h1>Home Page</h1>
+        </>
+      )}
+    </>
   );
 };
 
