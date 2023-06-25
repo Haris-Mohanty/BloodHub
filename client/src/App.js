07d2c3d376501bdaa88fdf9316d1977516644a22
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-25 16:51:51 +0530

    client- src- components- Routes- ProtectedRoutes.js- route protected.

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,18 @@ import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ProtectedRoutes from "./components/Routes/ProtectedRoutes";
 function App() {
   return (
     <>
       <ToastContainer />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={
+            <ProtectedRoutes>
+              <HomePage />
+            </ProtectedRoutes>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
