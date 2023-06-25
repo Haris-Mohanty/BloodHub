2885f9081983441140745c1188a0daf2ecaac156
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-25 16:43:33 +0530

    client- src- components- Routes- ProtectedRoutes.js - Get current user...

---
+++ client/src/components/Routes/ProtectedRoutes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+
+const ProtectedRoutes = ({ children }) => {
+  const dispatch = useDispatch();
+
+  //Get User
+  const 
+  return <div></div>;
+};
+
+export default ProtectedRoutes;
