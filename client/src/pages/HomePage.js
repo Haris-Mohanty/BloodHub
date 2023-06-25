53d57516f12e81ee44990178ab83ff0fbabe80f1
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-25 17:52:03 +0530

    client- src- components- shared- Form- Register.js- Show spinner in register page code fixed.

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const HomePage = () => {
+  const {loader}
   return (
     <div>
       <h1>Home Page</h1>
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,25 +1,30 @@
 import React from "react";
 import Form from "../../components/shared/Form/Form";
 import { useSelector } from "react-redux";
+import Spinner from "../../components/shared/Spinner";
+import { toast } from "react-toastify";
 
 const Register = () => {
   const { loading, error } = useSelector((state) => state.auth);
   return (
     <>
-      <div className="container-fuild">
-        <div className="row g-0">
-          <div className="col-md-8 form-banner">
-            <img src="./assets/images/register.jpg" />
-          </div>
-          <div className="col-md-4 shadow-sm form-container">
-            <Form
-              formTitle={"Register Page"}
-              submitBtn={"Register"}
-              formType={"register"}
-            />
+      {error && <span>{toast.error(error)}</span>}
+      {loading ? ( <Spinner /> ) : (
+        <div className="container-fuild">
+          <div className="row g-0">
+            <div className="col-md-8 form-banner">
+              <img src="./assets/images/register.jpg" />
+            </div>
+            <div className="col-md-4 shadow-sm form-container">
+              <Form
+                formTitle={"Register Page"}
+                submitBtn={"Register"}
+                formType={"register"}
+              />
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
