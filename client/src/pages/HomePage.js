1ca9cd0ef3068b528d6a8657fb0eb82e117deaf8
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-26 17:02:47 +0530

    client- src- components- Modal show code processing....

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -13,8 +13,14 @@ const HomePage = () => {
         <Spinner />
       ) : (
         <>
-          <h4><i className="fa-solid fa-plus"></i>
-            Add Inventory</h4>
+          <h4
+            className="main-page"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+          >
+            <i className="fa-solid fa-plus"></i>
+            Add Inventory
+          </h4>
         </>
       )}
     </Layout>
