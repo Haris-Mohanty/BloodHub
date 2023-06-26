526f2ba3b4f7536ab3165e37ed88a72baaafa443
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-26 16:29:57 +0530

    client- src- components- shared- Layout- Menu- userMenu.js- User menu created successfully.

---
+++ client/src/components/shared/Layout/Menus/userMenus.js
@@ -0,0 +1,17 @@
+export const UserMenu = [
+  {
+    name: "Inventory",
+    path: "/",
+    icon: "fa-solid fa-warehouse",
+  },
+  {
+    name: "Donor",
+    path: "/donor",
+    icon: "fa-solid fa-hand-holding-medical",
+  },
+  {
+    name: "Hospital",
+    path: "/hospital",
+    icon: "fa-solid fa-hospital",
+  },
+];
