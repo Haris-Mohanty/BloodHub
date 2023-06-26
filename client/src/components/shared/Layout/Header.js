0d29f8bf3bb3a7b9d81b5943b3173351d99b736d
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-26 15:17:09 +0530

    client- src- components- shared- Layout- Header.js- Nav bar design Processing.....

--- client/src/components/shared/Layout/Header.js
+++ client/src/components/shared/Layout/Header.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <div>
-      
+      <div className="bg-light shadow-sm container-fluid">
+        <nav className="navbar navbar-expand-sm fixed-top shadow-lg navbar-light bg-white">
+          <div className="container">
+            <a href="#" className="navbar-brand">
+              <img src="./assets/images/nav.png" className="w-50" /> 
+            </a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar">
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="navbar-collapse collapse" id="myNavbar">
+              <ul className="navbar-nav w-100 justify-content-end">
+                <li className="nav-item">
+                  <a href="#" className="nav-link">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="#" className="nav-link">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="#" className="nav-link">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="#" className="nav-link">
+                    About
+                  </a>
+                </li>
+                <div className="dropdown btn-group shadow-sm ml-auto">
+                  <button className="btn">
+                    <i className="fa fa-user" data-bs-toggle="dropdown" />
+                    <div className="dropdown-menu">
+                      <a href="#" className="dropdown-item">
+                        <i className="fa fa-user"></i>
+                        Register
+                      </a>
+                      <a href="#" className="dropdown-item">
+                        <i className="fa fa-sign-in"></i>
+                        Login
+                      </a>
+                    </div>
+                  </button>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- client/src/components/shared/Layout/Layout.js
+++ client/src/components/shared/Layout/Layout.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
     <>
-      <div className="header">Header</div>
+      <div className="header">
+        <Header />
+      </div>
       <div className="content">{children}</div>
     </>
   );
