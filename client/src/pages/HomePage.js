628c8074275b33e2455e382a183673768fd55796
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-26 14:26:37 +0530

    client- src- components- shared- Layout- Layout.js created successfully

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,11 +2,12 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Spinner from "../components/shared/Spinner";
 import { toast } from "react-toastify";
+import Layout from "../components/shared/Layout/Layout";
 
 const HomePage = () => {
   const { loading, error } = useSelector((state) => state.auth);
   return (
-    <>
+    <Layout>
       {error && <span>{toast.error(error)}</span>}
       {loading ? (
         <Spinner />
