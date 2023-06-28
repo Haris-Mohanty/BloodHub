06f7004b9ab1455681486c2a20220378fb6e352e
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-28 19:27:54 +0530

    npm i moment- installed for timestamp showing formateed.

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,6 +5,7 @@ import { toast } from "react-toastify";
 import Layout from "../components/shared/Layout/Layout";
 import Modal from "../components/shared/modal/Modal";
 import API from "../services/API";
+import moment from "moment";
 
 const HomePage = () => {
   const { loading, error } = useSelector((state) => state.auth);
