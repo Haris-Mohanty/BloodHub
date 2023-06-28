5537faf24faf7dd2f52a2f01ce63578ec8fa54a5
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-28 19:17:12 +0530

    client- src- pages- HomePage.js- Table design processing...

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -48,7 +48,7 @@ const HomePage = () => {
           <table className="table table-striped">
             <thead>
               <tr>
-                <th scope="col">Sl No.</th>
+                <th scope="col">Blood Group</th>
                 <th scope="col">First</th>
                 <th scope="col">Last</th>
                 <th scope="col">Handle</th>
@@ -60,18 +60,6 @@ const HomePage = () => {
                 <td>Mark</td>
                 <td>Otto</td>
                 <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
               </tr>
             </tbody>
           </table>
