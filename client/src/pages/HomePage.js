a112668370b9bb84c5aba456a3f27d22df7767c4
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-28 19:16:06 +0530

    client- src- pages- HomePage.js- Add table to show blood records...

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -10,7 +10,7 @@ const HomePage = () => {
   const { loading, error } = useSelector((state) => state.auth);
 
   // **Show records in page ***/
-  const [ data, setData ] = useState();
+  const [data, setData] = useState([]);
   //get
   const getBloodrecords = async () => {
     try {
@@ -37,13 +37,45 @@ const HomePage = () => {
       ) : (
         <>
           <h4
-            className="main-page mt-5"
+            className="main-page mt-5 mb-5"
             data-bs-toggle="modal"
             data-bs-target="#staticBackdrop"
           >
             <i className="fa-solid fa-plus"></i>
             Add Inventory
           </h4>
+
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Sl No.</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+
           <Modal />
         </>
       )}
