7757d766b051db2863b00dcc13d0d4aaa2f4d0a6
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-29 19:46:05 +0530

    client- src- pages- dashboard- Donor.js- Show donors data in page code fixed.

--- client/src/pages/dashboard/Donor.js
+++ client/src/pages/dashboard/Donor.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../../components/shared/Layout/Layout";
 import API from "../../services/API";
+import moment from "moment";
 
 const Donor = () => {
-  const { data, setData } = useState([]);
+  const [data, setData] = useState([]);
   //find donor records
   const getDonors = async () => {
     try {
       const { data } = await API.get("/inventory/get-donors");
-      //   console.log(data);
-      if (data?.success) {
-        setData(data?.donors);
-      }
+    //   console.log(data);
+        if (data?.success) {
+          setData(data?.donors);
+        }
     } catch (error) {
       console.log(error);
     }
@@ -36,9 +37,15 @@ const Donor = () => {
           </tr>
         </thead>
         <tbody className="category-list">
-          <tr>
-            <td></td>
-          </tr>
+          {data?.map((record, index) => (
+            <tr key={record._id}>
+              <td>{record._id}</td>
+              <td>{record.name || record.organisationName + " (Org) "}</td>
+              <td>{record.email}</td>
+              <td>{record.phone}</td>
+              <td>{moment(record.createdAt).format("DD/MM/YYYY hh:mm A")}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Layout>
