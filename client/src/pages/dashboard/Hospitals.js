09004f032a47d1516c5125f6a601f281460c6606
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-29 22:36:39 +0530

    client- src- pages- dashboard- Hospital.js- Table desidned successfully.

--- client/src/pages/dashboard/Hospitals.js
+++ client/src/pages/dashboard/Hospitals.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../../components/shared/Layout/Layout";
 import API from "../../services/API";
+import moment from "moment";
 
 const Hospitals = () => {
   const [data, setData] = useState([]);
@@ -33,6 +34,7 @@ const Hospitals = () => {
             <th scope="col">Name</th>
             <th scope="col">Email</th>
             <th scope="col">MobileNo. </th>
+            <th scope="col">Address </th>
             <th scope="col">Date & Time</th>
           </tr>
         </thead>
@@ -40,8 +42,9 @@ const Hospitals = () => {
           {data?.map((record, index) => (
             <tr key={record._id}>
               <td>{index + 1}</td>
-              <td>{record.name || record.organisationName + "(ORG)"}</td>
+              <td>{record.hospitalName}</td>
               <td>{record.email}</td>
+              <td>{record.phone}</td>
               <td>{record.phone}</td>
               <td>{moment(record.createdAt).format("DD/MM/YYYY hh:mm A")}</td>
             </tr>
