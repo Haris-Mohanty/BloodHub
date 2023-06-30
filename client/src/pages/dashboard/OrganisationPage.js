7704701267f90230d5f3af481ae376d3e986477c
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-30 13:23:30 +0530

    client- src- pages- dashboard- Organisation.js- Get organisations data code processing....

--- client/src/pages/dashboard/OrganisationPage.js
+++ client/src/pages/dashboard/OrganisationPage.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./../../components/shared/Layout/Layout";
 
 const OrganisationPage = () => {
+  const [data, setData] = useState([]);
+
+  //find org data
+  const getOrganisations = async () => {
+    
+  };
   return (
     <Layout>
       <h2 className="mt-5 mb-4 fw-bold text-primary">Organisation Details</h2>
