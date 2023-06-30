97747d36125b3dddf31001e83ffc8d0191924b7c
Haris Mohanty <harismohanty8658@gmail.com>
2023-06-30 13:28:08 +0530

    client- src- pages- dashboard- Organisation.js- Get organisations data successfully.

--- client/src/pages/dashboard/OrganisationPage.js
+++ client/src/pages/dashboard/OrganisationPage.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./../../components/shared/Layout/Layout";
+import API from "../../services/API";
 
 const OrganisationPage = () => {
   const [data, setData] = useState([]);
 
   //find org data
   const getOrganisations = async () => {
-    
+    try {
+      const { data } = await API("/inventory/get-organisation");
+        if (data?.success) {
+          setData(data?.organisations);
+        }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  useEffect(() => {
+    getOrganisations();
+  }, []);
+
   return (
     <Layout>
       <h2 className="mt-5 mb-4 fw-bold text-primary">Organisation Details</h2>
