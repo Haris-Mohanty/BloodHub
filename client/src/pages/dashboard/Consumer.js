f45d93c8b9cf53e12305e2ef0ba588ef83a08b86
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-01 09:17:27 +0530

    client- src- pages- dashboard- Consumer.js- Show records in table code processing...

--- client/src/pages/dashboard/Consumer.js
+++ client/src/pages/dashboard/Consumer.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Layout from "../../components/shared/Layout/Layout";
 import API from "../../services/API";
 import { useSelector } from "react-redux";
+import moment from "moment";
 
 const Consumer = () => {
   //get current user
@@ -31,7 +32,29 @@ const Consumer = () => {
   }, []);
   return (
     <Layout>
-      <h1>Consumer Details</h1>
+      <h1 className="mt-5 mb-4 fw-bold text-success">Consumer Details</h1>
+      <hr />
+
+      <table className="table table-striped">
+        <thead>
+          <tr className="green">
+            <th scope="col">S/N</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">MobileNo. </th>
+            <th scope="col">Date & Time</th>
+          </tr>
+        </thead>
+        <tbody className="category-list">
+          <tr>
+            <td></td>
+
+            <td></td>
+
+            <td>{moment(record.createdAt).format("DD/MM/YYYY hh:mm A")}</td>
+          </tr>
+        </tbody>
+      </table>
     </Layout>
   );
 };
