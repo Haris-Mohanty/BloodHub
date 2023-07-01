f11049ff2a12e2e27b45f3f64fec4075ead82245
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-01 09:16:08 +0530

    client- src- pages- dashboard- Consumer.js- get data from data base code fixed.

--- client/src/pages/dashboard/Consumer.js
+++ client/src/pages/dashboard/Consumer.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/shared/Layout/Layout";
+import API from "../../services/API";
+import { useSelector } from "react-redux";
 
 const Consumer = () => {
+  //get current user
+  const { user } = useSelector((state) => state.auth);
+
   const [data, setData] = useState([]);
   //find donor records
   const getDonors = async () => {
     try {
-      const { data } = await API.get("/inventory/get-donors");
+      const { data } = await API.post("/inventory/get-inventory-hospital", {
+        filters: {
+          inventoryType: "out",
+          hospital: user?._id,
+        },
+      });
       //   console.log(data);
       if (data?.success) {
         setData(data?.donors);
