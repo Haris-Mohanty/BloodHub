8e0d1c6a8fe6b2a1bcc3e437699d1346f5a18396
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-01 09:12:26 +0530

    client- src- pages- dashboard- Consumer.js- get data from data base code processing...

--- client/src/pages/dashboard/Consumer.js
+++ client/src/pages/dashboard/Consumer.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import Layout from '../../components/shared/Layout/Layout'
+import React from "react";
+import Layout from "../../components/shared/Layout/Layout";
 
 const Consumer = () => {
+  const [data, setData] = useState([]);
+  //find donor records
+  const getDonors = async () => {
+    try {
+      const { data } = await API.get("/inventory/get-donors");
+      //   console.log(data);
+      if (data?.success) {
+        setData(data?.donors);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getDonors();
+  }, []);
   return (
     <Layout>
       <h1>Consumer Details</h1>
     </Layout>
-  )
-}
+  );
+};
 
-export default Consumer
+export default Consumer;
