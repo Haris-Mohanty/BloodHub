51960c2639681f38d571c825e1b5cc0736658482
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-02 15:30:54 +0530

    server- routes- analyticsRoutes.js- Data showing successfully.

--- client/src/pages/dashboard/Analytics.js
+++ client/src/pages/dashboard/Analytics.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from "react";
 import Header from "../../components/shared/Layout/Header";
 import API from "./../../services/API";
-// import Layout from "../../components/shared/Layout/Layout";
 
 const Analytics = () => {
   const [data, setData] = useState([]);
+  const colors = [
+    "#884A39",
+    "#C38154",
+    "#FFC26F",
+    "#22A299",
+    "#4F709C",
+    "#4942E4",
+    "#0079FF",
+    "#FF0060",
+    "#22A699",
+  ];
   //Get Blood Group Data
   const getBloodGroupData = async () => {
     try {
       const { data } = await API.get("/analytics/bloodGroups-data");
       if (data?.success) {
-        setData(data?.BloodGroupData);
+        setData(data?.bloodGroupData);
         console.log(data);
       }
     } catch (error) {
@@ -24,7 +34,31 @@ const Analytics = () => {
   return (
     <>
       <Header />
-      <h1 className="mt-5">Analytics Page</h1>
+
+      <div className="row mt-4">
+        <div className="col-md-2"></div>
+        <div className="col-md-8 mt-5 d-flex flex-row flex-wrap">
+          {data?.map((record, i) => (
+            <div className="card m-2 p-1" key={i} style={{ width: "18rem", backgroundColor:`${colors[i]}` }}>
+              <div className="card-body">
+                <h1 className="card-title bg-light text-dark text-center mb-3 fw-bold">
+                  {record.bloodGroup}
+                </h1>
+                <p className="card-text text-light">
+                  Total In : <b>{record.totalIn}</b> (ML)
+                </p>
+                <p className="card-text text-light">
+                  Total Out : <b>{record.totalOut}</b> (ML)
+                </p>
+              </div>
+              <div className="card-footer text-light bg-dark text-center">
+                Total Available : <b>{record.availableBlood}</b> (ML)
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="col-md-2"></div>
+      </div>
     </>
   );
 };
