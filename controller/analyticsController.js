4e8a2000a01784a08e68d526f95bf48b7f4e231e
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-02 12:42:06 +0530

    server- controller- analyticsController.js- Create controller function for show blood group analytics...........

--- controller/analyticsController.js
+++ controller/analyticsController.js
@@ -1,4 +1,14 @@
 //******** GET BLOOD DATA *********/
-const bloodGroupDetailsController = async (req, res) => {};
+const bloodGroupDetailsController = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error in Blood Group Data Analytics API!",
+      error,
+    });
+  }
+};
 
 module.exports = { bloodGroupDetailsController };
