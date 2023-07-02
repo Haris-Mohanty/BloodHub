0bafd4c9cb9b697a0837e56360e204f31fd0330d
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-02 13:06:42 +0530

    server- controller- analyticsController.js- Create controller function for show blood group code fixed.

--- controller/analyticsController.js
+++ controller/analyticsController.js
@@ -1,6 +1,73 @@
+const inventoryModel = require("../models/inventoryModel");
+
 //******** GET BLOOD DATA *********/
 const bloodGroupDetailsController = async (req, res) => {
   try {
+    const bloodGroups = [
+      "O+",
+      "O-",
+      "AB",
+      "AB+",
+      "AB-",
+      "A+",
+      "B+",
+      "A-",
+      "B-",
+    ];
+    const bloodGroupData = [];
+    const organisation = req.body.userId;
+    //Get Signle Blood Group
+    await Promise.all(async (bloodGroup) => {
+      //Count Total "IN"
+      const totalIn = await inventoryModel.aggregate([
+        {
+          $match: {
+            bloodGroup: bloodGroup,
+            inventoryType: "in",
+            organisation,
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            total: { $sum: "$quantity" },
+          },
+        },
+      ]);
+      //Count Total "OUT"
+      const totalOut = await inventoryModel.aggregate([
+        {
+          $match: {
+            bloodGroup: bloodGroup,
+            inventoryType: "out",
+            organisation,
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            total: { $sum: "$quantity" },
+          },
+        },
+      ]);
+      //Calculate
+      const availableBlood =
+        (totalIn[0]?.total || 0) - (totalOut[0]?.total || 0);
+      //Push Data
+      bloodGroupData.push({
+        bloodGroup,
+        totalIn: totalIn[0]?.total || 0,
+        totalOut: totalOut[0]?.total || 0,
+        availableBlood,
+      });
+    });
+
+    //Res
+    return res.status(200).send({
+      success: true,
+      message: "Blood Group Data Fetched Successfully!",
+      bloodGroupData,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).send({
