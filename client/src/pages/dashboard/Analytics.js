cb04a15f31823a91f20c168a731435f82914f1d9
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-02 12:16:31 +0530

    Redirecting to analytics page successfully.

--- client/src/pages/dashboard/Analytics.js
+++ client/src/pages/dashboard/Analytics.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "../../components/shared/Layout/Header";
+// import Layout from "../../components/shared/Layout/Layout";
 
 const Analytics = () => {
   return (
     <>
       <Header />
-      <h1>Analytics Page</h1>
+      <h1 className="mt-5">Analytics Page</h1>
     </>
   );
 };
