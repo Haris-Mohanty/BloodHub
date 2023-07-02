4b8b1e999f9b6f807dc2b0e247365088aec2d331
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-02 12:36:08 +0530

    server- routes- analyticsRoutes.js- Routes created successfully(for get donors record.)

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
+const {
+  bloodGroupDetailsController,
+} = require("../controller/analyticsController");
 
 //Router Object
 const router = express.Router();
 
 //**** CREATE ROUTES ****/
-
+//Get Donor Records
+router.get("/get-donors", authMiddleware, bloodGroupDetailsController);
 
 module.exports = router;
