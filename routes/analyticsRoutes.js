dc387ed57e2f95fafb154239c899707863ee781e
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-02 12:33:03 +0530

    server- controller- Create analyticsController.js file to create a controller function to get Blood Records...

---
+++ routes/analyticsRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const authMiddleware = require("../middleware/authMiddleware");
+
+//Router Object
+const router = express.Router();
+
+//**** CREATE ROUTES ****/
+
+
+module.exports = router;
