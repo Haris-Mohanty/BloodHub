32d6f06d53d50481132b0e0b9eae83efebdbefb1
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-03 10:28:02 +0530

    client- src- pages- Admin-  HospitalList.js- Code processing....

---
+++ client/src/pages/Admin/HospitalList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/shared/Layout/Layout";
+
+const HospitalList = () => {
+  return (
+    <Layout>
+      <h1>Hospital List</h1>
+    </Layout>
+  );
+};
+
+export default HospitalList;
