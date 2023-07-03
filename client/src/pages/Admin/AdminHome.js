a9074b9ef369f013dd04051d516f454ada4a9d9a
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-03 10:46:54 +0530

    client- src- pages- Admin-  AdminHome.js- Showing Admin name successfully.

--- client/src/pages/Admin/AdminHome.js
+++ client/src/pages/Admin/AdminHome.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import Layout from '../../components/shared/Layout/Layout'
+import React from "react";
+import Layout from "../../components/shared/Layout/Layout";
+import { useSelector } from "react-redux";
 
 const AdminHome = () => {
+  const { user } = useSelector((state) => state.auth);
   return (
     <Layout>
-      <div className='container'>
-        <div className='d-flex'>
-            <h1>Welcome Admin</h1>
+      <div className="container mt-4">
+        <div className="d-flex">
+          <h1>
+            Welcome Admin: <i> {user?.name}</i>
+          </h1>
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default AdminHome
+export default AdminHome;
