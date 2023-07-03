afe250dd058de2d668abbe1b7736bee7a9bf00a0
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-03 10:29:29 +0530

    client- src- pages- Admin-  OrganisationList.js- Code processing....

---
+++ client/src/pages/Admin/OrganisationList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/shared/Layout/Layout";
+
+const OrganisationList = () => {
+  return (
+    <Layout>
+      <h1>Organisation List</h1>
+    </Layout>
+  );
+};
+
+export default OrganisationList;
