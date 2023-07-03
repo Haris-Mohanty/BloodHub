f04bf73e1019ba059dd2c886271255c99cf1790e
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-03 10:26:29 +0530

    client- src- pages- Admin-  DonorList.js- Code processing..........

---
+++ client/src/pages/Admin/DonorList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "./../../components/shared/Layout/Layout";
+
+const DonorList = () => {
+  return (
+    <Layout>
+      <h>Donor List</h>
+    </Layout>
+  );
+};
+
+export default DonorList;
