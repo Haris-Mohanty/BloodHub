4a8c7a5c6ebb0afe612a36f45a0efd35be5a9a79
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-04 13:57:30 +0530

    client- src- pages- Admin- DonorList.js- Table created to show donor list.

--- client/src/pages/Admin/DonorList.js
+++ client/src/pages/Admin/DonorList.js
@@ -8,7 +8,10 @@ const DonorList = () => {
   const getDonors = async () => {
     try {
       const { data } = await API.get("/admin/donor-list");
-      console.log(data);
+      if (data?.success) {
+        setData(data?.donosr);
+        console.log(data);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -22,6 +25,23 @@ const DonorList = () => {
     <Layout>
       <h1 className="mt-5 mb-4 fw-bold text-success font-sofias">Donor List</h1>
       <hr />
+
+      <table className="table table-striped font-Lato">
+        <thead>
+          <tr className="green">
+            <th scope="col">S/N</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">MobileNo. </th>
+            <th scope="col">Date & Time</th>
+          </tr>
+        </thead>
+        <tbody className="category-list">
+          <tr>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
     </Layout>
   );
 };
