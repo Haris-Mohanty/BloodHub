93c67c856230339491be623b0e953970707785af
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-04 13:54:48 +0530

    client- src- pages- Admin- DonorList.js- Donor details getting successfully.

--- client/src/pages/Admin/DonorList.js
+++ client/src/pages/Admin/DonorList.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./../../components/shared/Layout/Layout";
+import API from "../../services/API";
 
 const DonorList = () => {
+  const [data, setData] = useState([]);
+  //Get Donor Details
+  const getDonors = async () => {
+    try {
+      const { data } = await API.get("/admin/donor-list");
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getDonors();
+  }, []);
+
   return (
     <Layout>
       <h1 className="mt-5 mb-4 fw-bold text-success font-sofias">Donor List</h1>
-      <hr/>
+      <hr />
     </Layout>
   );
 };
