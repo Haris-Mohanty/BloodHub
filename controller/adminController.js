3cbe3d05c1378dd839c1fbd6b60b08c19cfb0eda
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-04 12:51:37 +0530

    server- controller- adminController.js- Code processing..

--- controller/adminController.js
+++ controller/adminController.js
@@ -2,6 +2,11 @@ const getDonorsListController = async (req, res) => {
   try {
   } catch (error) {
     console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error in Donor List API!",
+      error,
+    });
   }
 };
 
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
 const { getDonorsListController } = require("../controller/adminController");
+const adminMiddleware = require("../middleware/adminMiddleware");
 
 //Router Object
 const router = express.Router();
 
 //Routes
 //GET DONOR LIST
-router.get("/donor-list", authMiddleware, getDonorsListController);
+router.get("/donor-list", authMiddleware, adminMiddleware, getDonorsListController);
 
 //Export
 module.exports = router;
