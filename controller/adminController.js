846f6651c0f86670f99ea1353f48aaf999b6f00a
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-04 12:59:27 +0530

    server- controller- adminController.js- Code fixed.(Donor get successfully.)

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,5 +1,16 @@
+const userModel = require("../models/userModel");
+
 const getDonorsListController = async (req, res) => {
   try {
+    const donorData = await userModel
+      .find({ role: "donor" })
+      .sort({ createdAt: -1 });
+    return res.status(200).send({
+      success: false,
+      totalCount: donorData.length,
+      message: "Donor List Fetched Successfully!",
+      donorData,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).send({
