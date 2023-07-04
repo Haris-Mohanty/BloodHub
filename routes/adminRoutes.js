58b2eb31b4dcc56302b6be73a2ec4af6e0f6fd21
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-04 12:40:01 +0530

    server- routes- adminRoutes.js- Create routes code fixed.

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const authMiddleware = require("../middleware/authMiddleware");
+const { getDonorsListController } = require("../controller/adminController");
 
 //Router Object
 const router = express.Router();
 
 //Routes
 //GET DONOR LIST
-router.get("/donor-list", )
+router.get("/donor-list", authMiddleware, getDonorsListController);
 
 //Export
