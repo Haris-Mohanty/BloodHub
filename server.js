4bbbc78696e45183af2a7383d5973090a26c1887
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-05 15:36:06 +0530

    Path created (build)

--- server.js
+++ server.js
@@ -27,6 +27,14 @@ app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
 app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
+//STATIC FILES
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+//STATIC ROUTES
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 //***** PORT *****/
 const PORT = process.env.PORT || 8080;
 
