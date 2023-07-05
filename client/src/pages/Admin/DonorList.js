2914c1c91b40466e8aa6f2347f79a1e85e1011a6
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-05 15:06:59 +0530

    server- controller- adminController.js- Delete donor successfully.

--- client/src/pages/Admin/DonorList.js
+++ client/src/pages/Admin/DonorList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Layout from "./../../components/shared/Layout/Layout";
 import API from "../../services/API";
 import moment from "moment";
+import Swal from "sweetalert2";
 
 const DonorList = () => {
   const [data, setData] = useState([]);
@@ -23,7 +24,52 @@ const DonorList = () => {
   }, []);
 
   ///******* DELETE DONOR RECORD ******/
-  
+  const handleDelete = (id) => {
+    try {
+      const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: "btn btn-success mx-2",
+          cancelButton: "btn btn-danger",
+        },
+        buttonsStyling: false,
+      });
+
+      swalWithBootstrapButtons
+        .fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonText: "Yes, delete it!",
+          cancelButtonText: "No, cancel!",
+          reverseButtons: true,
+        })
+        .then(async (result) => {
+          if (result.isConfirmed) {
+            await API.delete(`/admin/delete-donor/${id}`);
+            setTimeout(() => {
+              window.location.reload();
+            }, 1000);
+            swalWithBootstrapButtons.fire(
+              "Deleted!",
+              "Your file has been deleted.",
+              "success"
+            );
+          } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+          ) {
+            swalWithBootstrapButtons.fire(
+              "Cancelled",
+              "Your imaginary file is safe :)",
+              "error"
+            );
+          }
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Layout>
@@ -53,7 +99,14 @@ const DonorList = () => {
               <td>{record.phone}</td>
               <td>{record.address}</td>
               <td>{moment(record.createdAt).format("DD/MM/YYYY hh:mm A")}</td>
-              <td><button className="btn btn-danger py-2"><i className="fa-solid fa-trash"></i>Delete</button></td>
+              <td>
+                <button
+                  className="btn btn-danger py-2"
+                  onClick={() => handleDelete(record._id)}
+                >
+                  <i className="fa-solid fa-trash"></i>Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
